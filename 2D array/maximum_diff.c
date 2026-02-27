a358368a7bfb9f99d8a7f7f9cbcf7e96e33356a1
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-02-27 22:47:39 +0530

    Add files via upload

---
+++ 2D array/maximum_diff.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int sum[r];
+    for(int i=0;i<r;i++){
+        sum[i]=0;
+        for(int j=0;j<c;j++){
+            sum[i]=sum[i]+arr[i][j];
+        }
+    }
+    int max=0;
+    for(int i=0;i<r;i++){
+        for(int j=i+1;j<r;j++){
+            int diff=sum[i]-sum[j];
+            if(diff<0){
+                diff=-diff;
+            }
+            if(diff>max){
+                max=diff;
+            }
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
