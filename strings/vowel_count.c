905cc069a7e13b4b61d196d12cd4052ddaaec877
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-03-01 22:17:38 +0530

    Add files via upload

---
+++ strings/vowel_count.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[20];
+    scanf("%s",str);
+    int i=0;
+    int count=0;
+    while(str[i]!='\0'){
+        if(str[i]=='a'|| str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
+            count++;
+        }
+        i++;
+    }
+    printf("%d",count);
+    return 0;
+}
