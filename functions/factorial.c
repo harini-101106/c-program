8d31cdb842e8611f45cf85b08abc0a9b07fe3449
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-03-02 15:44:50 +0530

    Add files via upload

---
+++ functions/factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int fact(int num){
+    int fact=1;
+    for(int i=1;i<=num;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",fact(n));
+    return 0;
+}
